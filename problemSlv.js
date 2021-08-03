5dd95f0a88cffba03805224af21e8f911ce943ad
CesarR21 <88210226+CesarR21@users.noreply.github.com>
2021-08-03 11:57:31 -0500

    Add files via upload

---
+++ problemSlv.js
@@ -0,0 +1,47 @@
+function reverseString(hello){
+   let strArr= hello.split("");     //turn string into array
+   let reverseStrArr= strArr.reverse();
+   let reverseString= reverseStrArr.join("");
+   return(reversedStr)
+
+   reverseString(hello)
+}
+//method for reverse array
+let word=["hello"];
+console.log(word.reverse())
+
+//capitalizeWords ("elements can be one lineded")
+
+function capitalizeWord(helloworld){
+let word=helloWorld.split(' ').map(word=>{
+  let firstLetter= word.slice(0,1);
+  let remaining=word.slice(1);
+  firstLetter = firstLetter.toUpperCase();
+  
+  return '${firstLetter}${remaining}';
+});
+ return word.join("");
+
+  //  function capitalizeWord(helloworld){let word=helloWorld.split(' ').map(word=>{
+     //return word.charAt(0).toUpperCase()+word.slice(1);
+
+     //
+// STRING COMPRESS.
+
+   function stringCompression(aaabbbccc){
+      let str="aaabbbccc";
+      let compString= '';
+      let currentLetter=str[i]
+      let curCount=1
+      for(let i=0;i<str.lenght;i++)
+      whiile(str[i+1]===currentLetter);{
+         curCount ++;
+         1++
+      stringCompression(aaabbbccc);{
+     return compString
+
+      }
+   }
+}
+
+
